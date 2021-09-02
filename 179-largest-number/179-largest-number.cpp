49360039492c6e287002c23554a5db7af7a86792
Narcos101 <70684116+Narcos101@users.noreply.github.com>
2021-09-02 10:43:03 +0530

    Time: 23 ms (7.04%), Space: 11.4 MB (38.75%) - LeetHub

--- 179-largest-number/179-largest-number.cpp
+++ 179-largest-number/179-largest-number.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    string largestNumber(vector<int> &num) {
-        vector<string> arr;
-        for(auto i:num)
+    string largestNumber(vector<int>& nums) {
+        vector<string>arr;
+        for(auto i: nums){
             arr.push_back(to_string(i));
-        sort(begin(arr), end(arr), [](string &s1, string &s2){ return s1+s2>s2+s1; });
-        string res;
-        for(auto s:arr)
-            res+=s;
-        while(res[0]=='0' && res.length()>1)
-            res.erase(0,1);
-        return  res;
-    }
+        }
+        string res = "";
+        sort(arr.begin(),arr.end(),[&](string a,string b){ return a+b > b+a ;});
+        for(auto i: arr){
+            res += i;
+        }
+        while(res.size() > 1 && res[0] == '0'){
+            res.erase(0,1); 
+        }
+        return res;
+    } 
 };
