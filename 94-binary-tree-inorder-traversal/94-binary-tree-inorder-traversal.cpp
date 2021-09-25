441f9714f7ff5d20e9497c320f3f627dcea4621b
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2021-09-25 18:06:12 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (64.17%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -1,33 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    public:
-        vector<int> inorderTraversal(TreeNode* root) {
-            stack<TreeNode *>st;
-            vector<int>ans;
-            TreeNode *node = root;
-            while(true){
-                if(node != NULL){
-                    st.push(node);
-                    node = node->left;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inorder; 
+        
+        TreeNode* cur = root; 
+        while(cur != NULL) {
+            cout << cur->val << endl;
+            if(cur->left == NULL) {
+                inorder.push_back(cur->val); 
+                cur = cur->right; 
+            }
+            else {
+                TreeNode* prev = cur->left; 
+                while(prev->right != NULL && prev->right != cur) {
+                    prev = prev->right; 
                 }
-                else{
-                    if(st.empty()) break;
-                    node = st.top();
-                    st.pop();
-                    ans.push_back(node->val);
-                    node = node->right;
+                
+                if(prev->right == NULL) {
+                    prev->right = cur;
+                    cur = cur->left; 
+                }
+                else {
+                    prev->right = NULL; 
+                    inorder.push_back(cur->val); 
+                    cur = cur->right; 
                 }
             }
-            return ans;
         }
+        return inorder; 
+    }
 };
