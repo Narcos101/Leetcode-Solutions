bc213c84590610f4cdad2ab5390597c07182fbba
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2021-09-25 14:46:27 +0530

    Time: 48 ms (70.13%), Space: 29.1 MB (91.00%) - LeetHub

---
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.cpp
@@ -0,0 +1,56 @@
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        string s = "";
+        if(!root) return s;
+        queue<TreeNode *>q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode *node = q.front();
+            q.pop();
+            if(node == NULL) s.append("#,");
+            else{
+                s.append(to_string(node->val)+",");
+            }
+            if(node != NULL){
+                q.push(node->left);
+                q.push(node->right);
+            }
+        }
+        return s;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if(data.size() == 0) return NULL; 
+        stringstream s(data);
+        string str; 
+        getline(s, str, ',');
+        TreeNode *root = new TreeNode(stoi(str));
+        queue<TreeNode*> q; 
+        q.push(root); 
+        while(!q.empty()) {
+            TreeNode *node = q.front();
+            q.pop();
+            getline(s,str,',');
+            if(str == "#"){
+                node->left = NULL;
+            }
+            else{
+                node->left = new TreeNode(stoi(str));
+                q.push(node->left);
+            }
+            getline(s,str,',');
+            if(str == "#"){
+                node->right = NULL;
+            }
+            else{
+                node->right = new TreeNode(stoi(str));
+                q.push(node->right);
+            }
+        }
+        return root; 
+    }
+};
