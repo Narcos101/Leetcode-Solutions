24f1b063f94e54c0086df7f0801df94fe921a096
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2022-01-15 13:44:41 +0530

    Time: 40 ms (77.01%), Space: 34 MB (49.53%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
-    unordered_set<int> vis;
-bool canReach(vector<int>& arr, int st) {
-    if (st >= 0 && st < arr.size() && vis.insert(st).second) {
-        return arr[st] == 0 ||
-            canReach(arr, st + arr[st]) || canReach(arr, st - arr[st]);
+    bool canReach(vector<int>& arr, int start) {
+        if(start<0 || start>=arr.size() || arr[start]<0){
+            return false;
+        }
+        if(arr[start]==0) return true;
+        
+        arr[start]= -arr[start];
+        
+        bool ans1 =  canReach(arr,start+arr[start]);
+        bool ans2 =  canReach(arr,start-arr[start]);
+        
+        arr[start]= -arr[start];// change data back to orginal 
+        
+        return ans1 || ans2;
+        
+        
     }
-    return false;
-}
 };
