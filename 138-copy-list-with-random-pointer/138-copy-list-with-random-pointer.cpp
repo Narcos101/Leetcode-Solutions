2435a66c22ccbbb0c5b5fdc5f52b41efee60b1ce
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2022-03-12 12:55:12 +0530

    Time: 21 ms (12.99%), Space: 11.5 MB (8.60%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -17,21 +17,24 @@ public:
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
+        // traverse throught the list, if the node doesn't exist make one 
+        // As the values are not unique we cannot store the value of a particular node as an identifier
+        // In this case we will store the address of the original node and store the clone nodes address
+        // to check whether the copy of the original node has been created or not 
+        
         map<Node*,Node*>mpp;
-        Node *head2 = head;
-        while(head2 != NULL){
-            mpp[head2] = new Node(head2->val);
-            head2 = head2->next;
+        Node* head2 = head;
+        while(head != NULL){
+            Node *node = new Node(head->val);
+            mpp[head] = node;
+            head = head->next;
         }
-        head2 = head;
-        Node *head3 = mpp[head2];
-        while(head2!=NULL){
+        head = head2;
+        while(head2 != NULL){
             mpp[head2]->next = mpp[head2->next];
             mpp[head2]->random = mpp[head2->random];
             head2 = head2->next;
         }
-        
-        mpp.clear();
-        return head3;
+        return mpp[head];
     }
 };
