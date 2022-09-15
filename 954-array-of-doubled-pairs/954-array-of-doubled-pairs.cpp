3d47c5de7f8ef044fb38aa23195373794fd28cc7
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2022-09-15 12:11:31 +0530

    Time: 325 ms (16.56%), Space: 57.9 MB (93.91%) - LeetHub

--- 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
+++ 954-array-of-doubled-pairs/954-array-of-doubled-pairs.cpp
@@ -6,35 +6,20 @@ public:
         for(int i = 0; i < n;i++){
             mpp[arr[i]]++;
         }
-        sort(arr.begin(),arr.end());
+        sort(arr.begin(),arr.end(),[&](int a,int b){
+            return abs(a) < abs(b);
+        });
         for(int i = 0; i < n;i++){
             int val = arr[i];
-            if(val < 0){
-                if(mpp[val] != 0){
-                    mpp[val]--;
-                    if(val % 2){
-                        return false;
-                    }
-                    if(mpp[val/2] != 0){
-                        mpp[val/2]--;
-                    }
-                    else{
-                        return false;
-                    }
-                } 
+            if(mpp[val]){
+                mpp[val]--;
+                if(mpp[val * 2] != 0){
+                    mpp[val * 2]--;
+                }
+                else{
+                    return false;
+                }
             }
-            else{
-                if(mpp[val] != 0){
-                    mpp[val]--;
-                    if(mpp[val * 2] != 0){
-                        mpp[val * 2]--;
-                    }
-                    else{
-                        return false;
-                    }
-                }    
-            }
-            
         }
         return true;
     }
