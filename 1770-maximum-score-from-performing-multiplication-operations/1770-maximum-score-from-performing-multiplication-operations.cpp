3d83fcd83bf0b8b50b1aca9d818b1d8e5b0c09fe
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2022-09-17 12:32:24 +0530

    Time: 682 ms (51.81%), Space: 119.9 MB (72.06%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-        int n = (int) nums.size(), m = (int) multipliers.size();
-        vector<int> dp(m + 1);
-        for (int i = 0; i < m; i++) {
-            int mult = multipliers[m - 1 - i];
-            for (int j = 0; j < m - i; j++) {
-                dp[j] = max(
-                    mult * nums[j] + dp[j + 1], 
-                    mult * nums[j + (n - (m - i))] + dp[j]
-                );
+    int maximumScore(vector<int>& nums, vector<int>& mul) {
+        int n = nums.size();
+        int m = mul.size();
+        vector<vector<int>>dp(m+1,vector<int>(m+1,0));
+        for(int i = m-1; i >= 0;i--){
+            for(int j = i; j >= 0;j--){
+                dp[i][j] = max(nums[j] * mul[i] + dp[i+1][j+1], nums[n-1-(i-j)] * mul[i] + dp[i+1][j]);
             }
         }
-        return dp[0];
+        return dp[0][0];
     }
 };
