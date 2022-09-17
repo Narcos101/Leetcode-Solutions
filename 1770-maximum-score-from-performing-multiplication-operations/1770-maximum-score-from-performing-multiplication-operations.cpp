d4b63f31f9385a7296f6befbebc0d3da074ce4fd
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2022-09-17 12:40:25 +0530

    Time: 488 ms (82.72%), Space: 119.1 MB (73.68%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -3,12 +3,13 @@ public:
     int maximumScore(vector<int>& nums, vector<int>& mul) {
         int n = nums.size();
         int m = mul.size();
-        vector<vector<int>>dp(m+1,vector<int>(m+1,0));
+        vector<int>dp(m+1,0);
         for(int i = m-1; i >= 0;i--){
+            vector<int>newdp = dp;
             for(int j = i; j >= 0;j--){
-                dp[i][j] = max(nums[j] * mul[i] + dp[i+1][j+1], nums[n-1-(i-j)] * mul[i] + dp[i+1][j]);
+                dp[j] = max(nums[j] * mul[i] + newdp[j+1], nums[n-1-(i-j)] * mul[i] + newdp[j]);
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 };
