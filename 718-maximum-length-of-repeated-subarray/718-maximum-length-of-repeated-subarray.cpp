da45ecbe1d952c9d34f7c810b02c820f33187781
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2022-09-20 12:41:42 +0530

    Time: 289 ms (85.45%), Space: 108.4 MB (61.43%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -7,9 +7,9 @@ public:
         int i = 0;
         int j = 0;
         int ans = 0;
-        vector<int>dp(n+1,0);
         vector<int>pre(n+1,0);
         for(int i = 1; i <= n;i++){
+            vector<int>dp(n+1,0);
             for(int j = 1; j <= m;j++){
                 if(nums1[i-1] == nums2[j-1]){
                     dp[j] = 1 + pre[j-1];
