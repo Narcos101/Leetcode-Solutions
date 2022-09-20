547c449f1464ed87c43276b4f8dd65fde14c52ce
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2022-09-20 12:13:25 +0530

    Time: 569 ms (39.40%), Space: 11.9 MB (92.72%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -7,28 +7,20 @@ public:
         int i = 0;
         int j = 0;
         int ans = 0;
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        calc(nums1,nums2,i,j,n,m,ans,dp);
+        vector<int>dp(n+1,0);
+        vector<int>pre(n+1,0);
+        for(int i = 1; i <= n;i++){
+            for(int j = 1; j <= m;j++){
+                if(nums1[i-1] == nums2[j-1]){
+                    dp[j] = 1 + pre[j-1];
+                }
+                else{
+                    dp[j] = 0;
+                }
+                ans = max(ans,dp[j]);
+            }
+            pre = dp;
+        }
         return ans;
     }
-    
-    int calc(vector<int>&nums1,vector<int>&nums2,int i,int j,int n,int m,int&ans,vector<vector<int>>&dp){
-        if(i >= n || j >= m){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int con1 = 0;
-        int con2 = 0;
-        int con3 = 0;
-        if(nums1[i] == nums2[j]){
-            con1 = 1 + calc(nums1,nums2,i+1,j+1,n,m,ans,dp);
-            ans = max(ans,con1);
-        }      
-        calc(nums1,nums2,i+1,j,n,m,ans,dp);
-        calc(nums1,nums2,i,j+1,n,m,ans,dp);
-        dp[i][j] = max(con1,dp[i][j]);
-        return dp[i][j];
-    }    
 };
