f409d0d1c863bc4e99503eb095ee4d7a973d9a70
Vivek Jaiswar <70684116+Narcos101@users.noreply.github.com>
2022-10-02 13:10:41 +0530

    Time: 144 ms (11.44%), Space: 8.6 MB (28.55%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,31 +1,37 @@
 class Solution {
 public:
     int mod = 1e9+7;
-    int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>>dp(n+1,vector<int>(target+1,-1));
-        return calc(n,k,target,dp);
+    int numRollsToTarget(int n, int nk, int target) {
+        vector<vector<int>>dp(n+1,vector<int>(target+1,0));
+        dp[0][0] = 1;
+        for(int i = 1; i <= n;i++){
+            for(int k = 1; k <= target;k++){
+                int ans = 0;
+                for(int j = 1; j <= nk;j++){
+                    if(j <= k){
+                        ans = (ans % mod + dp[i-1][k-j] % mod) % mod;
+                    }
+                }
+                dp[i][k] = ans;
+            }
+        } 
+        return dp[n][target];
     }
+};    
     
-    
-    int calc(int n,int k,int target,vector<vector<int>>&dp){
-        if(n == 0){
-            if(target == 0){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        }
-        if(dp[n][target] != -1){
-            return dp[n][target];
-        }
-        int ans = 0;
-        for(int i = 1; i <= k;i++){
-            if(i <= target){
-                ans = (ans % mod + calc(n-1,k,target-i,dp) % mod) % mod;
-            }
-        }
-        return dp[n][target] = ans;
-    }
-
-};
+//     int calc(int n,int k,int target,vector<vector<int>>&dp){
+//         if(n == 0){
+//             if(target == 0){
+//                 return 1;
+//             }
+//             else{
+//                 return 0;
+//             }
+//         }
+//         if(dp[n][target] != -1){
+//             return dp[n][target];
+//         }
+//         int ans = 0;
+        
+//         return dp[n][target] = ans;
+//     }
